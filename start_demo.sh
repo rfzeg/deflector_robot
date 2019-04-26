5d98f52cf3203d427288b80dec317336fc1c80b2
Roberto Zegers <rzegers@uc.cl>
2019-04-26 13:08:22 +0000

    Add bash script to start demo, split demo.launch in many files

---
+++ start_demo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Deflector Robot Simulation and example command algorithm"
+echo "Make sure you have sourced devel/setup.bash"
+echo ""
+xterm  -e  "roslaunch deflector_robot gazebo.launch" &
+sleep 6
+
+xterm  -e  "roslaunch deflector_robot spawn_robot.launch" & 
+sleep 6
+
+xterm  -e  "roslaunch deflector_robot ctrl_manager.launch" & 
+
