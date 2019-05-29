cd5b11b1d09e6944f2594f511f5868ccbd66e1cc
Roberto Zegers <rzegers@puc.cl>
2019-05-29 06:51:54 -0700

    Add wooden cubes on table top for demo purposes

--- start_demo.sh
+++ start_demo.sh
@@ -9,7 +9,11 @@ xterm  -e  "roslaunch deflector_robot spawn_robot.launch" &
 sleep 4
 
 # Spawn table
-xterm  -e  "roslaunch deflector_robot spawn_sdf_models.launch" & 
+xterm  -e  "roslaunch deflector_robot spawn_sdf_table.launch" & 
+sleep 4
+
+# Spawn cubes
+xterm  -e  "roslaunch deflector_robot spawn_sdf_cubes.launch" & 
 sleep 4
 
 xterm  -e  "roslaunch deflector_robot ctrl_manager.launch" & 
